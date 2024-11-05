32f371d8d9c95868a015028296ce4973573a1a76
Geethika Kolukuluri <97678018+Geethikavarma@users.noreply.github.com>
2024-11-05 17:04:06 -0600

    Added artifacts class, pug, and endpoint

--- app.js
+++ app.js
@@ -6,11 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const artifactsRouter = require('./routes/artifacts');
-const gridRouter = require('./grid'); // Adjust the path accordingly
-
-
-
 
 var app = express();
 
@@ -23,10 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', gridRouter);
-
-app.use('/artifacts', artifactsRouter);
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
